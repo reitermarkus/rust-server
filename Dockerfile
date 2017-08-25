2cf60947a9717b039cfb41abee8d41b0c016a2cf
Markus Reiter <me@reitermark.us>
2017-08-26 01:16:05 +0200

    Use `pre_build` hook.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM scratch
 
 COPY target/x86_64-unknown-linux-musl/release/server /
-CMD ["/server"]
+
+ENTRYPOINT ["/server"]
 
 COPY www /www
--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf target
-
-alias rust-musl-builder='docker run --rm -it -v "$(pwd):/home/rust/src" ekidd/rust-musl-builder'
-
-rust-musl-builder cargo build --release
-
-docker build . -t reitermarkus/server
