09b83c91a506ad649341556c83d782ff6e634d16
Markus Reiter <me@reitermark.us>
2018-09-09 16:29:59 +0200

    Update dependencies.

--- src/main.rs
+++ src/main.rs
@@ -5,35 +5,32 @@ extern crate chrono;
 
 use iron::prelude::*;
 use iron::{Handler, IronResult};
-
 use staticfile::Static;
 use mount::Mount;
 
+use chrono::Utc;
+
 use std::env;
 use std::str::FromStr;
-
+use std::net::{SocketAddrV4, Ipv4Addr};
 use std::path::Path;
 
 static DEFAULT_PORT: u16 = 3000;
 static DEFAULT_WEBROOT: &str = "/www";
 
 fn main() {
-  let port = match env::var("PORT") {
-    Ok(val) => u16::from_str(&val).unwrap(),
-    Err(_) => DEFAULT_PORT,
-  };
-
-  let webroot_str = env::var("WEBROOT").unwrap_or(String::from(DEFAULT_WEBROOT));
+  let port = env::var("PORT").ok().and_then(|p| u16::from_str(&p).ok()).unwrap_or(DEFAULT_PORT);
+  let webroot_str = env::var("WEBROOT").unwrap_or(DEFAULT_WEBROOT.to_string());
   let webroot = Path::new(&webroot_str);
 
-  println!("Starting server on port {} in {:?} …", port, webroot);
+  println!("Starting server on port {} in {} …", port, webroot.to_string_lossy());
 
   let handler = Static::new(webroot);
 
   let log_request = move |req: &mut Request| -> IronResult<Response> {
     let path = req.url.path().join("/");
 
-    let time = chrono::Utc::now().to_rfc3339();
+    let time = Utc::now().to_rfc3339();
     println!("{} - {} /{}", time, req.method, path);
     handler.handle(req)
   };
@@ -41,6 +38,8 @@ fn main() {
   let mut mount = Mount::new();
   mount.mount("/", log_request);
 
-  let bind_addr = format!("0.0.0.0:{}", port);
-  Iron::new(mount).http(bind_addr).unwrap();
+  let bind_addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), port);
+  Iron::new(mount).http(bind_addr).expect("Failed to start server");
+
+  println!("Server started.");
 }
